266d62106c0ef93cf562f64ce63afc13c706a68a
Rawi Baransy <rawi_baransy@berkeley.edu>
2024-08-22 13:37:31 -0700

    newtons method

--- newton.py
+++ newton.py
@@ -1,16 +1,21 @@
 def function(x):
     return x**2
 
+
 def fist(x):
     delta = 0.0001
     first = 0
-    first = ((function(x+delta) - function(x)) / delta)
-    return first 
+    first = (function(x + delta) - function(x)) / delta
+    return first
+
+
 def second(x):
     delta = 0.0001
     second = 0
-    second = ((fist(x+delta) - fist(x)) / delta)
-    return second 
+    second = (fist(x + delta) - fist(x)) / delta
+    return second
+
+
 def Newton(start, function):
     x = start
     delta = 0.0001
@@ -18,12 +23,11 @@ def Newton(start, function):
 
     while abs(x_t - x) > delta:
         copy = x_t
-        
+
         first_derivative = fist(x)
         second_derivative = second(x)
-        x_t = x - first_derivative/second_derivative
-        
-        x= copy
-        
+        x_t = x - first_derivative / second_derivative
+
+        x = copy
+
         print(x_t)
-        
